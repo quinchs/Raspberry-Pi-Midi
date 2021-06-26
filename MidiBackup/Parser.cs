f93f14a5ec506b6750bba46c661c60bf29e78d32
quin lynch <lynchquin@gmail.com>
2021-06-26 06:30:34 -0300

    Fix formatting

--- MidiBackup/Parser.cs
+++ MidiBackup/Parser.cs
@@ -8,105 +8,105 @@ namespace MidiBackup
 {
     public class Parser
     {
-		public IEnumerable<MidiMessage> Parse(byte[] bytes, int index, int size)
-		{
-			int i = index;
-			int end = index + size;
-			while (i < end)
-			{
-				if (bytes[i] == 0xF0)
-				{
-					yield return ConvertBuff(0xF0, 0, 0, bytes.Skip(i + 1).ToArray(), i, size - i - 1, bytes);
-					i += size;
-				}
-				else
-				{
-					var z = FixedDataSize(bytes[i]);
-					if (end < i + z)
-						throw new Exception(string.Format(
-							"Received data was incomplete to build MIDI status message for '{0:X}' status.",
-							bytes[i]));
-					yield return ConvertBuff(bytes[i],
-						(byte)(z > 0 ? bytes[i + 1] : 0),
-						(byte)(z > 1 ? bytes[i + 2] : 0),
-						bytes.Skip(i).Take(z + 1).ToArray(), i, z + 1, bytes);
-					i += z + 1;
-				}
-			}
-		}
-
-		private MidiMessage ConvertBuff(byte type, byte arg1, byte arg2, byte[] data, int dataOffset, int dataLength, byte[] raw)
+        public IEnumerable<MidiMessage> Parse(byte[] bytes, int index, int size)
         {
-			var value = type + (arg1 << 8) + (arg2 << 16);
-
-			var packet = new MidiPacket()
-			{
-				Data = data,
-				DataLength = dataLength,
-				DataOffset = dataOffset,
-				Raw = raw,
-				Value = value
-			};
-
-			if(type < 0xF0)
+            int i = index;
+            int end = index + size;
+            while (i < end)
             {
-				// removes the channel bit, idk if theres a better way to do this, (0xB5 >> 4 << 4 == 0xB0)
-				switch ((type >> 4) << 4)
-				{
-					case (byte)StatusType.NoteOff or (byte)StatusType.NoteOn:
-						return new NoteMessage(packet);
+                if (bytes[i] == 0xF0)
+                {
+                    yield return ConvertBuff(0xF0, 0, 0, bytes.Skip(i + 1).ToArray(), i, size - i - 1, bytes);
+                    i += size;
+                }
+                else
+                {
+                    var z = FixedDataSize(bytes[i]);
+                    if (end < i + z)
+                        throw new Exception(string.Format(
+                            "Received data was incomplete to build MIDI status message for '{0:X}' status.",
+                            bytes[i]));
+                    yield return ConvertBuff(bytes[i],
+                        (byte)(z > 0 ? bytes[i + 1] : 0),
+                        (byte)(z > 1 ? bytes[i + 2] : 0),
+                        bytes.Skip(i).Take(z + 1).ToArray(), i, z + 1, bytes);
+                    i += z + 1;
+                }
+            }
+        }
 
-					case (byte)(StatusType.Program):
-						return new ProgramMessage(packet);
+        private MidiMessage ConvertBuff(byte type, byte arg1, byte arg2, byte[] data, int dataOffset, int dataLength, byte[] raw)
+        {
+            var value = type + (arg1 << 8) + (arg2 << 16);
 
-					case (byte)StatusType.CC:
-						{
-							switch ((CCType)arg1)
-							{
-								case CCType.Hold:
-									return new SustainMessage(packet);
+            var packet = new MidiPacket()
+            {
+                Data = data,
+                DataLength = dataLength,
+                DataOffset = dataOffset,
+                Raw = raw,
+                Value = value
+            };
 
-								default: return new DefaultMidiMessage(packet);
-							}
-						}
-					default: return new DefaultMidiMessage(packet);
-				}
-			}
+            if (type < 0xF0)
+            {
+                // removes the channel bit, idk if theres a better way to do this, (0xB5 >> 4 << 4 == 0xB0)
+                switch ((type >> 4) << 4)
+                {
+                    case (byte)StatusType.NoteOff or (byte)StatusType.NoteOn:
+                        return new NoteMessage(packet);
+
+                    case (byte)(StatusType.Program):
+                        return new ProgramMessage(packet);
+
+                    case (byte)StatusType.CC:
+                        {
+                            switch ((CCType)arg1)
+                            {
+                                case CCType.Hold:
+                                    return new SustainMessage(packet);
+
+                                default: return new DefaultMidiMessage(packet);
+                            }
+                        }
+                    default: return new DefaultMidiMessage(packet);
+                }
+            }
             else
             {
                 switch (type)
                 {
-					case 0xF0:
-						return new SystemExclusiveMessage(packet);
+                    case 0xF0:
+                        return new SystemExclusiveMessage(packet);
 
-					default: return new DefaultMidiMessage(packet);
-				}
+                    default: return new DefaultMidiMessage(packet);
+                }
             }
-			
+
         }
 
 
-		public byte FixedDataSize(byte statusByte)
-		{
-			switch ((StatusType)(statusByte & 0xF0))
-			{
-				case StatusType.SysEx1: // and 0xF7, 0xFF
-					switch ((StatusType)statusByte)
-					{
-						case StatusType.MtcQuarterFrame:
-						case StatusType.SongSelect:
-							return 1;
-						case StatusType.SongPositionPointer:
-							return 2;
-						default:
-							return 0; // no fixed data
-					}
-				case StatusType.Program:
-				case StatusType.CAf:
-					return 1;
-				default:
-					return 2;
-			}
-		}
-	}
+        public byte FixedDataSize(byte statusByte)
+        {
+            switch ((StatusType)(statusByte & 0xF0))
+            {
+                case StatusType.SysEx1: // and 0xF7, 0xFF
+                    switch ((StatusType)statusByte)
+                    {
+                        case StatusType.MtcQuarterFrame:
+                        case StatusType.SongSelect:
+                            return 1;
+                        case StatusType.SongPositionPointer:
+                            return 2;
+                        default:
+                            return 0; // no fixed data
+                    }
+                case StatusType.Program:
+                case StatusType.CAf:
+                    return 1;
+                default:
+                    return 2;
+            }
+        }
+    }
 }
